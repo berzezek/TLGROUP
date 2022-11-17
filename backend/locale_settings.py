3dd921726222188160ad0ac9ac3733e53923d616
berzezek <wknduz@gmail.com>
2022-11-17 10:02:04 +0300

    from deploy

--- backend/locale_settings.py
+++ backend/locale_settings.py
@@ -1,11 +1,34 @@
+from pathlib import Path
+import sys
+from .settings import DATABASES_AVAILABLE
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'cn73530_tlgroup',
-#         'USER': 'cn73530_tlgroup',
-#         'PASSWORD': 'Aa20102010',
-#         'HOST': '188.225.40.227',
-#         'PORT': '3306',
-#     }
-# }
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['tlgroup.cn73530.tmweb.ru']
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / '../dist/'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+DATABASES = {}
+if 'test' in sys.argv:
+    DATABASES['default'] = DATABASES_AVAILABLE['test']
+
+else:
+    DATABASES['default'] = DATABASES_AVAILABLE['remote']
+
+
